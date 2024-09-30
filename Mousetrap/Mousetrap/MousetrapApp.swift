557f4e803c329db8d1a677ee794459bb7c814ce3
HishamZ25 <hmza224@uky.edu>
2024-09-30 11:34:40 -0400

    First Mousetrap Commit

---
+++ Mousetrap/Mousetrap/MousetrapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MousetrapApp.swift
+//  Mousetrap
+//
+//  Created by Hisham Zannoun on 9/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MousetrapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
